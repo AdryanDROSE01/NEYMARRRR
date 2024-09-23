26010d9973101981f79088639038c2a058a18472
AdryanDROSE01 <160235604+AdryanDROSE01@users.noreply.github.com>
2024-09-23 07:53:05 -0300

    Add files via upload

---
+++ demarvelous/perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'mvp',
+    'QUEM É O YOUNGEST MVP?',
+    'D.ROSE'
+)
+
+criaCartao(
+    'BEST TITLE',
+    'WHAT IS THE BEST TITLE FROM CHICAGO?',
+    '1996'
+)
+
+criaCartao(
+    'THE RODMAMBA',
+    'THE WORM?',
+    'THE CHOOSEN ONE'
+)
